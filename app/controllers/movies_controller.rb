3f482cf968e895ebb8a57e891a22e07b98413cab
Ubuntu <ubuntu@domU-12-31-39-04-21-4C.compute-1.internal>
2012-03-19 02:24:04 +0000

    hw2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,19 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort] #|| session[:sort]
+    case sort
+    when 'title'
+      ordering,@title_header = {:order => :title}, 'hilite'
+    when 'release_date'
+      ordering,@date_header = {:order => :release_date}, 'hilite'
+    end
+
+#    if params[:sort] != session[:sort]
+#      session[:sort] = sort
+#      redirect_to :sort => sort and return
+#    end
+    @movies = Movie.all(ordering)
   end
 
   def new
